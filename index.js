41cc5d8e397c68f8cc3d540f5d6178d46824a9f9
Sergiu Ghenciu <1024gs@gmail.com>
2019-09-05 14:18:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+ * Copy all the content into browser's console to see the result
+ *
+ * It transforms a CSV string into a list of key-value objects
+ */
+
+const merge = (objects) => Object.assign(...objects);
+
+const pick = (columns) => (xs) =>
+  merge(columns.map((x) => ({[x.name]: xs[x.index]})));
+
+const readFile = (fileName) => 'Bob,32\nAlice,25\n';
+
+const parserFactory = ({delimiter, columns}) => (str) =>
+  str
+    .trim()
+    .split('\n')
+    .map((x) => x.split(delimiter))
+    .map(pick(columns));
+
+const parseNameAndAge = parserFactory({
+  delimiter: ',',
+  columns: [{name: 'name', index: 0}, {name: 'age', index: 1}],
+});
+
+parseNameAndAge(readFile('students.csv'));
